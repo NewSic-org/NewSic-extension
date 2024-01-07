063e875ff5f2788569830ba0dabb6768986e6cfa
gSayak <gsayak22@gmail.com>
2024-01-07 14:10:09 +0530

    extension

---
+++ extension/scripts/contents.js
@@ -0,0 +1,16 @@
+const title = document.querySelector('h1.title');
+
+if(title){
+    title.style.color = 'red';
+}
+
+const title2 = document.querySelector('h1.artTitle.font_faus');
+if(title2){
+    title2.style.color = 'blue';
+}
+
+const article = document.querySelector('article');
+if(article){
+    article.style.backgroundColor = 'yellow';
+}
+
